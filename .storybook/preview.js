8d0477becd0f1436b40cfaecb9e89dcd5fe41ef9
Chris Chuang <chrischuang0421@gmail.com>
2021-08-17 10:17:50 +0800

    Build a simple component.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import "../src/index.css";
+
+// parameters are typically used to control the behavior of Storybook's features and addons.
 export const parameters = {
+  //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
